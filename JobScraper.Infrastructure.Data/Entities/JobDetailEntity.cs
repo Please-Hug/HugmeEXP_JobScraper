80b9d24712d776a02aab1e6b1b13288fe5f3bf3e
Huey S. Jeong <wjdgnltkd123@gmail.com>
2025-07-17 23:21:52 +0900

    feat: JobDetail 및 JobListingEntity 클래스 추가 및 수정 JobDetail 클래스의 RequiredSkills 속성을 IEnumerable<string>으로 변경하고, 새로운 JobDetailEntity 및 JobListingEntity 클래스를 추가했습니다.

--- JobScraper.Infrastructure.Data/Entities/JobDetailEntity.cs
+++ JobScraper.Infrastructure.Data/Entities/JobDetailEntity.cs
@@ -1,10 +1,12 @@
-﻿namespace JobScraper.Core.Models;
+﻿namespace JobScraper.Infrastructure.Data.Entities;
 
-public class JobDetail : JobListing
+public class JobDetailEntity
 {
     public required string Description { get; set; }
-    public required IEnumerable<string> RequiredSkills { get; set; } = [];
+    public required string[] RequiredSkills { get; set; } = [];
     public required long MinSalary { get; set; }
     public required long MaxSalary { get; set; }
     public required string Location { get; set; }
+    
+    public JobListingEntity JobListing { get; set; }
 }
---
+++ JobScraper.Infrastructure.Data/Entities/JobListingEntity.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobScraper.Infrastructure.Data.Entities;
+
+public class JobListingEntity
+{
+    [Key]
+    public required string Id { get; set; }
+    public required string Title { get; set; }
+    public required string Company { get; set; }
+    public required string Url { get; set; }
+    public required string Source { get; set; }
+    
+    public JobDetailEntity? JobDetail { get; set; }
+}
